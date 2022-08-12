c4dac782b405ddb885fa90276bff629bc4742073
JohnLGlucode <110444940+JohnLGlucode@users.noreply.github.com>
2022-08-12 07:27:22 +0200

    Update RecipeService.kt

--- app/src/main/java/com/example/recipe/retrofit/RecipeService.kt
+++ app/src/main/java/com/example/recipe/retrofit/RecipeService.kt
@@ -10,14 +10,14 @@ import retrofit2.http.Query
 interface RecipeService {
     // API Key: 98dd4bfc7ddf4feea2e902eff23e1a43
     @GET("/recipes/complexSearch")
-    fun searchRecipes(@Query("apiKey") apiKey: String, @Query("query") query: String): RecipeSearchResponse
+    suspend fun searchRecipes(@Query("apiKey") apiKey: String, @Query("query") query: String): RecipeSearchResponse
 
     @GET("/recipes/{id}/information")
-    fun getRecipeInformation(@Path("id") id: Int, @Query("apiKey") apiKey: String, @Query("includeNutrition") includeNutrition: Boolean): Recipe
+    suspend fun getRecipeInformation(@Path("id") id: Int, @Query("apiKey") apiKey: String, @Query("includeNutrition") includeNutrition: Boolean): Recipe
 
     @GET("/recipes/autocomplete")
-    fun getRecipeAutocomplete(@Query("apiKey") apiKey: String, @Query("number") number: Int, @Query("query") query: String): List<Recipe>
+    suspend fun getRecipeAutocomplete(@Query("apiKey") apiKey: String, @Query("number") number: Int, @Query("query") query: String): List<Recipe>
 
     @GET("/recipes/random")
-    fun getRandomRecipe(@Query("apiKey") apiKey: String, @Query("number") number: Int): RandomRecipeResponse
+    suspend fun getRandomRecipe(@Query("apiKey") apiKey: String, @Query("number") number: Int): RandomRecipeResponse
 }
